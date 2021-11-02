74f0670f810bd70cce2bbc2b65e2bd9bf7899ebd
KenYamada0509 <ken.yamada1@x-factory.jp>
2021-11-03 03:10:33 +0900

    Google Cloud SQL Auth Proxy download

--- setup.sh
+++ setup.sh
@@ -18,6 +18,10 @@ case ${OSTYPE} in
     ## https://iterm2.com/documentation-shell-integration.html
     ## https://www.rasukarusan.com/entry/2019/04/13/180443
     curl -L https://iterm2.com/misc/install_shell_integration.sh | bash
+    ## Google SQL Auth Proxy
+    ## https://cloud.google.com/sql/docs/mysql/sql-proxy?hl=ja#install
+    curl -o ~/.bin/cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
+    chmod +x ~/.bin/cloud_sql_proxy
     ;;
   linux*)
     # Linux用の設定
