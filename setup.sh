1628012cc009c6efdc548de8f94d0856dee2fd3b
Ken YAMADA <yamada.k@hokendohjin.co.jp>
2022-10-16 06:28:56 +0900

    change oh my zsh douloads

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ esac
 ## Oh my zsh install
 ## https://ohmyz.sh/
 echo 'Oh! my zsh install...'
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
