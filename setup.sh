65a30ce1c3089eb934b12a38958fe40077d2ae66
Ken YAMADA <yamada.k@hokendohjin.co.jp>
2022-10-16 04:18:59 +0900

    change setup

--- setup.sh
+++ setup.sh
@@ -5,11 +5,15 @@ DOT_FILES=(.bin .zshrc .zshenv .vimrc)
 
 for file in ${DOT_FILES[@]}
 do
+  mv $HOME/$file $HOME/$file.org
   ln -sf $DOTPATH/$file $HOME/$file
   if [ $? -eq 0 ]; then
     printf "    %-25s -> %s\n" "\$DOTPATH/$file" "\$HOME/$file"
   fi   
 done
+
+## anyenv setting
+git clone https://github.com/anyenv/anyenv ~/.anyenv
 
 case ${OSTYPE} in
   darwin*)
@@ -20,9 +24,10 @@ case ${OSTYPE} in
     defaults write com.apple.screencapture name "ss"
     mkdir $HOME/Pictures/ScreenShots/
     defaults write com.apple.screencapture location $HOME/Pictures/ScreenShots/
-    defaults write com.apple.dock springboard-columns -int 10;defaults write com.apple.dock springboard-rows -int 6;defaults write com.apple.dock ResetLaunchPad -bool TRUE;killall Dock
-    ## anyenv setting
-    git clone https://github.com/anyenv/anyenv ~/.anyenv
+    defaults write com.apple.dock springboard-columns -int 10
+    defaults write com.apple.dock springboard-rows -int 6
+    defaults write com.apple.dock ResetLaunchPad -bool TRUE
+    killall Dock
     ## mac packege init
     bash $DOTPATH/mac_init.sh
     ;;
@@ -33,7 +38,7 @@ esac
 
 # Google Cloud SDK install
 # https://cloud.google.com/sdk/docs/downloads-interactive?hl=ja#interactive_installation
-curl https://sdk.cloud.google.com | bash
+#curl https://sdk.cloud.google.com | bash
 
 # Firebase CLI
 # https://firebase.google.com/docs/cli#windows-standalone-binary
